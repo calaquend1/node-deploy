f05a39a98ff9b411939c172f2c917840e2a0ebb1
ivan_burilichev <burilichev@i-teco.ru>
2020-11-15 11:02:50 +0300

    create

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 80
+
+app.get('/', (request, response) => {
+	response.end(`
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+				</ul>
+			</nav>
+			<h1>Home page</h1>
+		</div>
+	`)
+})
+
+app.get('/about', (request, response) => {
+	response.end(`
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+				</ul>
+			</nav>
+			<h1>About page</h1>
+		</div>
+	`)
+})
+
+app.listen(port, (x) => console.log('server has been started', x))
